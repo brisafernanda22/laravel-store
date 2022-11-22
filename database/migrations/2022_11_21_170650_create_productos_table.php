d858fdd6b9856a36d018762af0dae4d1fde049a9
brisafernanda22 <brisafernankd@gmail.com>
2022-11-22 00:00:03 -0600

    api

--- database/migrations/2022_11_21_170650_create_productos_table.php
+++ database/migrations/2022_11_21_170650_create_productos_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('precio');
             $table->string('categoria');
             $table->string('image');
+            
         });
     }
 
